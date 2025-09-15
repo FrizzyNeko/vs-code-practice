7559c28b9232664ce4f7552b89e70fa3f94e0d22
asrin <asrin.senemoglu@gmail.com>
2025-09-15 12:03:46 +0300

    Refactor Amazon project: remove unused files, update script imports, and enhance cart functionality

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -1,3 +1,7 @@
+import { products } from '../data/products.js';
+import { cart } from '../data/cart.js';
+
+
 // Ürünler için HTML kodunu tutan bir degisken tanımla  
 let productsHTML = '';
 
@@ -27,7 +31,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-product-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -43,7 +47,7 @@ products.forEach((product) => {
 
       <div class="product-spacer"></div>
 
-      <div class="added-to-cart">
+      <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
@@ -59,37 +63,73 @@ products.forEach((product) => {
 // HTML kodunu sayfaya ekle
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+const cartTimers = {};
+
+function addToCart(productId) {
+  let matchingItem; // Aynı urunu sepete eklemek icin bir degisken tanimla
+
+  cart.forEach((cartItem) => {  // Her bir sepete ekle butonuna tıklandıgında
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+  // Urunun adetini al
+  const quantitySelector = document.querySelector(`.js-product-quantity-selector-${productId}`);
+  let quantityValue = Number(quantitySelector.value);
+
+  
+  if (matchingItem) { // Aynı urunu sepete eklemek icin
+    matchingItem.quantity += quantityValue;
+  } else { // Yeni urunu sepete eklemek icin
+    cart.push({
+      productId: productId,
+      quantity: quantityValue
+    });
+  }
+}
+
+function updateCartQuantity() {
+  // Sepet sayısını guncelle
+  let cartQuantity = 0;
+
+  cart.forEach((item) => { 
+    cartQuantity += item.quantity;
+  });
+  
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
+}
+
+function showAddedToCartMessage(productId) {
+   // "Added to cart" mesajını güncelle
+    const addedToCartElement = document.querySelector(`.js-added-to-cart-${productId}`);
+    addedToCartElement.style.opacity = 1;
+
+    // Önceki timer'ı temizle
+    if (cartTimers[productId]) {
+      clearTimeout(cartTimers[productId]);
+    }
+
+    // Yeni timer başlat
+    cartTimers[productId] = setTimeout(() => {
+      addedToCartElement.style.opacity = 0;
+      cartTimers[productId] = null; 
+    }, 2000);
+}
+
 // Urun sepete ekle
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => { // Her bir sepete ekle butonuna tıklandıgında
     button.addEventListener('click', () => {
       const productId = button.dataset.productId; // Urunun id'sini al
 
-      let matchingItem; // Aynı urunu sepete eklemek icin bir degisken tanimla
+      addToCart(productId);
 
-      cart.forEach((item) => {  // Her bir sepete ekle butonuna tıklandıgında
-        if (productId === item.productId) {
-          matchingItem = item;
-        }
-      });
+      updateCartQuantity();
 
-      if (matchingItem) { // Aynı urunu sepete eklemek icin
-        matchingItem.quantity += 1;
-      } else { // Yeni urunu sepete eklemek icin
-        cart.push({
-          productId: productId,
-          quantity: 1
-        });
-      }
+      showAddedToCartMessage(productId);
 
-      // Sepet sayısını guncelle
-      let cartQuantity = 0;
-
-      cart.forEach((item) => { 
-        cartQuantity += item.quantity;
-      });
-
-      document.querySelector('.js-cart-quantity')
-        .innerHTML = cartQuantity;
     });
   });
+
+
