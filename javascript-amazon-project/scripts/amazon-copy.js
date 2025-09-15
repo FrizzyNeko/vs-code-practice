51b79f9930508ab6d18e967081cf59ac0e3e5c5b
asrin <asrin.senemoglu@gmail.com>
2025-09-15 12:03:21 +0300

    Add Amazon project HTML and JavaScript files; enhance CSS for cart functionality

---
+++ javascript-amazon-project/scripts/amazon-copy.js
@@ -0,0 +1,116 @@
+// Ürünler için HTML kodunu tutan bir degisken tanımla  
+let productsHTML = '';
+
+// forEach() metodu kullanarak, daha önceden tanımlanmış products dizisindeki her bir ürün icin HTML kodunu olustur.
+products.forEach((product) => {
+  productsHTML += `
+    <div class="product-container">
+      <div class="product-image-container">
+        <img class="product-image"
+          src="${product.image}">
+      </div>
+
+      <div class="product-name limit-text-to-2-lines">
+        ${product.name}
+      </div>
+
+      <div class="product-rating-container">
+        <img class="product-rating-stars"
+          src="images/ratings/rating-${product.rating.stars * 10}.png"> 
+        <div class="product-rating-count link-primary">
+          ${product.rating.count}
+        </div>
+      </div>
+
+      <div class="product-price">
+        $${(product.priceCents / 100).toFixed(2) /* Urun fiyatini 2 ondalikli olarak formatla*/}
+      </div>
+
+      <div class="product-quantity-container">
+        <select class="js-product-quantity-selector-${product.id}">
+          <option selected value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+
+      <div class="product-spacer"></div>
+
+      <div class="added-to-cart js-added-to-cart-${product.id}">
+        <img src="images/icons/checkmark.png">
+        Added
+      </div>
+
+      <button class="add-to-cart-button button-primary js-add-to-cart"
+      data-product-id="${product.id}">
+        Add to Cart
+      </button>
+    </div>
+  `;
+});
+
+// HTML kodunu sayfaya ekle
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+// Urun sepete ekle
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => { // Her bir sepete ekle butonuna tıklandıgında
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId; // Urunun id'sini al
+
+      let matchingItem; // Aynı urunu sepete eklemek icin bir degisken tanimla
+
+      cart.forEach((item) => {  // Her bir sepete ekle butonuna tıklandıgında
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      });
+      // Urunun adetini al
+      const quantitySelector = document.querySelector(`.js-product-quantity-selector-${productId}`);
+      let quantityValue = Number(quantitySelector.value);
+
+      
+      if (matchingItem) { // Aynı urunu sepete eklemek icin
+        matchingItem.quantity += quantityValue;
+      } else { // Yeni urunu sepete eklemek icin
+        cart.push({
+          productId: productId,
+          quantity: quantityValue
+        });
+      }
+
+      // Sepet sayısını guncelle
+      let cartQuantity = 0;
+
+      cart.forEach((item) => { 
+        cartQuantity += item.quantity;
+      });
+      
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+
+      // "Added to cart" mesajını guncelle
+      document.querySelector(`.js-added-to-cart-${productId}`)
+      .classList.add('js-added-to-cart');
+      
+      const cartTimers ={};
+
+      cartTimers[productId] = setTimeout(() => {
+        document.querySelector(`.js-added-to-cart-${productId}`)
+        .classList.remove('js-added-to-cart');
+      }, 2000); 
+
+      clearTimeout(cartTimers[productId]);
+      
+
+    });
+  });
+
+
