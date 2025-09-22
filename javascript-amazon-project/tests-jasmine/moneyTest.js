b4561b80c043cbb70f2c7fe2bc09faf32458fb99
asrin <asrin.senemoglu@gmail.com>
2025-09-22 22:40:08 +0300

    docs: remove default tests and add money tests

---
+++ javascript-amazon-project/tests-jasmine/moneyTest.js
@@ -0,0 +1,23 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+describe("test suite: formatCurrency", () => {
+    it("converts cents into dollars", () => {
+        expect(formatCurrency(2095)).toBe("20.95");
+    });
+
+    it("works with 0", () => {
+        expect(formatCurrency(0)).toBe("0.00");
+    });
+
+    it("works with negative numbers", () => {
+        expect(formatCurrency(-2095)).toBe("-20.95");
+    });
+
+    it("works with floats", () => {
+        expect(formatCurrency(2095.5)).toBe("20.96");
+    });
+
+    it("works with floats with negative numbers", () => {
+        expect(formatCurrency(-2095.5)).toBe("-20.95");
+    });
+});
