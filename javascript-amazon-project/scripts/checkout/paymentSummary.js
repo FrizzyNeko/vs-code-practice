1b66f20edd264529617f880097dc26f42a6b7f88
asrin <asrin.senemoglu@gmail.com>
2025-09-22 21:43:04 +0300

    feat: save data and generate HTML for payment summary

---
+++ javascript-amazon-project/scripts/checkout/paymentSummary.js
@@ -0,0 +1,64 @@
+import { cart } from "../../data/cart.js";
+import { getProduct, products } from "../../data/products.js";
+import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+import { formatCurrency } from "../utils/money.js";
+import { renderOrderSummary } from "./orderSummary.js";
+
+// Ödeme bileşenini oluştur
+export function renderPaymentSummary() {
+    let productPriceCents = 0;
+    let shippingPriceCents = 0;
+    // Sepetteki ürünlerin fiyatlarını hesapla
+    cart.forEach((cartItem) => {
+        const product = getProduct(cartItem.productId); // Sepetteki ürünün bilgilerini al
+        productPriceCents +=product.priceCents * cartItem.quantity; // Ürünün fiyatını ürün adediyle çarp ve toplam fiyatı güncelle
+        
+        const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId); // Kargo seçeneğini al
+        shippingPriceCents += deliveryOption.priceCents; // Kargo fiyatını güncelle
+    });
+
+    const totalBeforeTaxCents = productPriceCents + shippingPriceCents; // Toplam fiyatı hesapla
+    const taxCents = Math.round(totalBeforeTaxCents * 0.18); // Vergiyi hesapla
+    const totalCents = totalBeforeTaxCents + taxCents; // Toplam fiyatı hesapla
+    
+    // Ödeme bileşenini oluştur
+    const paymentSummaryHTML = `
+        <div class="payment-summary-title">
+            Order Summary
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Items (3):</div>
+            <div class="payment-summary-money">$${formatCurrency(productPriceCents)} productPriceCents</div>
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Shipping &amp; handling:</div>
+            <div class="payment-summary-money">$${formatCurrency(shippingPriceCents)}</div>
+          </div>
+
+          <div class="payment-summary-row subtotal-row">
+            <div>Total before tax:</div>
+            <div class="payment-summary-money">$${formatCurrency(totalBeforeTaxCents)}</div>
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Estimated tax (10%):</div>
+            <div class="payment-summary-money">$${formatCurrency(taxCents)}</div>
+          </div>
+
+          <div class="payment-summary-row total-row">
+            <div>Order total:</div>
+            <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
+          </div>
+
+          <button class="place-order-button button-primary">
+            Place your order
+          </button>
+    `;
+
+    document.querySelector('.js-payment-summary')
+        .innerHTML = paymentSummaryHTML;
+
+}
+
