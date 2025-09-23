bda1b46820f9d234cc92796711cb16fec718c616
asrin <asrin.senemoglu@gmail.com>
2025-09-23 20:53:00 +0300

    refactor(chekout): convert from promises to async await

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -5,18 +5,34 @@ import { loadProductsFetch } from '../data/products.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
+async function loadPage () {
+  await loadProductsFetch();
+
+  await new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage();
+
+/*
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve) => {
     loadCart(() => {
       resolve();
     });
-  }),
+  })
 
 ]).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
 
 /*
 new Promise((resolve) => {
