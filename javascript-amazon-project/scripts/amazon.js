b58cf897dcbdbfeda5e6f4e318ca76ca67dd24c4
asrin <asrin.senemoglu@gmail.com>
2025-09-23 18:46:06 +0300

    feat: load products from the backend

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -1,120 +1,124 @@
 import { cart, addToCart, calculateCartQuantity } from '../data/cart.js';
-import { products } from '../data/products.js';
+import { products, loadProducts } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
+loadProducts(renderProductsGrid);
 
-// Ürünler için HTML kodunu tutan bir degisken tanımla  
-let productsHTML = '';
+function renderProductsGrid() {
+  // Ürünler için HTML kodunu tutan bir degisken tanımla  
+  let productsHTML = '';
 
-// forEach() metodu kullanarak, daha önceden tanımlanmış products dizisindeki her bir ürün icin HTML kodunu olustur.
-products.forEach((product) => {
-  productsHTML += `
-    <div class="product-container">
-      <div class="product-image-container">
-        <img class="product-image"
-          src="${product.image}">
-      </div>
-
-      <div class="product-name limit-text-to-2-lines">
-        ${product.name}
-      </div>
-
-      <div class="product-rating-container">
-        <img class="product-rating-stars"
-          src="${product.getStarsUrl()}"> 
-        <div class="product-rating-count link-primary">
-          ${product.rating.count}
+  // forEach() metodu kullanarak, daha önceden tanımlanmış products dizisindeki her bir ürün icin HTML kodunu olustur.
+  products.forEach((product) => {
+    productsHTML += `
+      <div class="product-container">
+        <div class="product-image-container">
+          <img class="product-image"
+            src="${product.image}">
         </div>
+
+        <div class="product-name limit-text-to-2-lines">
+          ${product.name}
+        </div>
+
+        <div class="product-rating-container">
+          <img class="product-rating-stars"
+            src="${product.getStarsUrl()}"> 
+          <div class="product-rating-count link-primary">
+            ${product.rating.count}
+          </div>
+        </div>
+
+        <div class="product-price">
+          ${product.getPrice()}
+        </div>
+
+        <div class="product-quantity-container">
+          <select class="js-product-quantity-selector-${product.id}">
+            <option selected value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
+
+        ${product.extraInfoHTML()}
+
+        <div class="product-spacer"></div>
+
+        <div class="added-to-cart js-added-to-cart-${product.id}">
+          <img src="images/icons/checkmark.png">
+          Added
+        </div>
+
+        <button class="add-to-cart-button button-primary js-add-to-cart"
+        data-product-id="${product.id}">
+          Add to Cart
+        </button>
       </div>
-
-      <div class="product-price">
-        ${product.getPrice()}
-      </div>
-
-      <div class="product-quantity-container">
-        <select class="js-product-quantity-selector-${product.id}">
-          <option selected value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
-      </div>
-
-      ${product.extraInfoHTML()}
-
-      <div class="product-spacer"></div>
-
-      <div class="added-to-cart js-added-to-cart-${product.id}">
-        <img src="images/icons/checkmark.png">
-        Added
-      </div>
-
-      <button class="add-to-cart-button button-primary js-add-to-cart"
-      data-product-id="${product.id}">
-        Add to Cart
-      </button>
-    </div>
-  `;
-});
-
-// HTML kodunu sayfaya ekle
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
-
-const cartTimers = {};
-
-function updateCartQuantity() {
-  // Sepet sayısını guncelle
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => { 
-    cartQuantity += cartItem.quantity;
+    `;
   });
-  
-  document.querySelector('.js-cart-quantity')
-    .innerHTML = cartQuantity;
-}
 
-function showAddedToCartMessage(productId) {
-   // "Added to cart" mesajını güncelle
-    const addedToCartElement = document.querySelector(`.js-added-to-cart-${productId}`);
-    addedToCartElement.style.opacity = 1;
+  // HTML kodunu sayfaya ekle
+  document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-    // Önceki timer'ı temizle
-    if (cartTimers[productId]) {
-      clearTimeout(cartTimers[productId]);
-    }
+  const cartTimers = {};
 
-    // Yeni timer başlat
-    cartTimers[productId] = setTimeout(() => {
-      addedToCartElement.style.opacity = 0;
-      cartTimers[productId] = null; 
-    }, 2000);
-}
+  function updateCartQuantity() {
+    // Sepet sayısını guncelle
+    let cartQuantity = 0;
 
-// Urun sepete ekle
-document.querySelectorAll('.js-add-to-cart')
-  .forEach((button) => { // Her bir sepete ekle butonuna tıklandıgında
-    button.addEventListener('click', () => {
-      const productId = button.dataset.productId; // Urunun id'sini al
-
-      addToCart(productId);
-
-      updateCartQuantity();
-
-      showAddedToCartMessage(productId);
-
+    cart.forEach((cartItem) => { 
+      cartQuantity += cartItem.quantity;
     });
-  });
+    
+    document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
+  }
 
-  const cartQuantiy = calculateCartQuantity();
+  function showAddedToCartMessage(productId) {
+    // "Added to cart" mesajını güncelle
+      const addedToCartElement = document.querySelector(`.js-added-to-cart-${productId}`);
+      addedToCartElement.style.opacity = 1;
 
-  document.querySelector('.js-cart-quantity')
-    .innerHTML = cartQuantiy;
+      // Önceki timer'ı temizle
+      if (cartTimers[productId]) {
+        clearTimeout(cartTimers[productId]);
+      }
+
+      // Yeni timer başlat
+      cartTimers[productId] = setTimeout(() => {
+        addedToCartElement.style.opacity = 0;
+        cartTimers[productId] = null; 
+      }, 2000);
+  }
+
+  // Urun sepete ekle
+  document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => { // Her bir sepete ekle butonuna tıklandıgında
+      button.addEventListener('click', () => {
+        const productId = button.dataset.productId; // Urunun id'sini al
+
+        addToCart(productId);
+
+        updateCartQuantity();
+
+        showAddedToCartMessage(productId);
+
+      });
+    });
+
+    const cartQuantiy = calculateCartQuantity();
+
+    document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantiy;
+}
+
 
 
--- javascript-amazon-project/scripts/checkout/paymentSummary.js
+++ javascript-amazon-project/scripts/checkout/paymentSummary.js
@@ -29,7 +29,7 @@ export function renderPaymentSummary() {
 
           <div class="payment-summary-row">
             <div>Items (3):</div>
-            <div class="payment-summary-money">$${formatCurrency(productPriceCents)} productPriceCents</div>
+            <div class="payment-summary-money">$${formatCurrency(productPriceCents)}</div>
           </div>
 
           <div class="payment-summary-row">
